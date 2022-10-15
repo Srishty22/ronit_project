e8d746d0db7f84884945982d25a70cb5ba1345a6
Srishty22 <60563424+Srishty22@users.noreply.github.com>
2022-10-16 00:49:27 +0530

    Home + Login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './Login';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
+    <Home />
   </React.StrictMode>
 );
 
