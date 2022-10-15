3db5682c6e323ab61be5fe6600bf227553b2983f
Srishty22 <60563424+Srishty22@users.noreply.github.com>
2022-10-16 00:49:09 +0530

    Home + Login

--- src/App.js
+++ src/App.js
@@ -4,15 +4,3 @@ import './App.css';
 import React from 'react';
 import {Button} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <Button>hi</Button>
-      </header>
-    </div>
-  );
-}
-
-export default App;
